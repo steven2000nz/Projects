4037d5b7859cfd55a83fa2dd6a8d0fb3cd8c00bb
Steven Liu <steven.liu@catchsoftware.com>
2016-03-09 11:22:50 +1300

    Test Project

--- Test/MYOB/UnitTest/UnitTest.cs
+++ Test/MYOB/UnitTest/UnitTest.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace UnitTest
+﻿namespace UnitTest
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
     using Calculator.DataAccess;
     using Calculator.Interface;
     using Calculator.Manager;
@@ -12,44 +14,47 @@ namespace UnitTest
     using Castle.MicroKernel.Registration;
     using Castle.Windsor;
 
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
     using Rhino.Mocks;
 
     [TestClass]
     public class UnitTest
     {
-        static WindsorContainer container;
+        private static WindsorContainer container;
 
-        IFileReadAccess<InputData> fileReadInputData;
-
-        [ClassInitialize()]
+        [ClassInitialize]
         public static void ClassInit(TestContext context)
         {
-            /*
-            var configuration = new Configuration(new TypeMapFactory(), MapperRegistry.AllMappers());
-            var mappingEngine = new MappingEngine(configuration);
-            new ScriptAutomapperConfiguration(configuration);
-            var scriptManager = new ScriptManager();
+            var calculatorContainer = MockRepository.GenerateStub<ICalculatorContainer>();
+            calculatorContainer.Stub(stub => stub.RegisterComponents()).Return(GetTestContainer());
 
-            fieldStorage = MockRepository.GenerateStub<IFieldStorage>();
-            fieldStorage.Stub(stub => stub.ExtractFieldsFromInstance(null)).IgnoreArguments().Return(new AbstractFieldData[] {});
-
-            comparisonBuilder = MockRepository.GenerateStub<ICustomFieldsPropertyComparisonBuilder>();
-            */
-
-            
+            container = calculatorContainer.RegisterComponents();
         }
 
-        [ClassCleanup()]
+        private static WindsorContainer GetTestContainer()
+        {
+            var testContainer = new WindsorContainer();
+
+            testContainer.Register(Component.For(typeof(IPayslipManager)).ImplementedBy(typeof(PayslipManager)).LifestyleTransient());
+            testContainer.Register(Component.For(typeof(ITaxManager)).ImplementedBy(typeof(TaxManager)).LifestyleTransient());
+            testContainer.Register(Component.For(typeof(IFileReadAccess<>)).ImplementedBy(typeof(CsvFileReadAccess<>)).Named(Container.CsvFileReadAccess).LifestyleTransient());
+            testContainer.Register(Component.For(typeof(IFileWriteAccess<>)).ImplementedBy(typeof(CsvFileWriteAccess<>)).Named(Container.CsvFileWriteAccess).LifestyleTransient());
+
+            return testContainer;
+        }
+
+        [ClassCleanup]
         public static void ClassCleanup()
         {
         }
 
-        [TestInitialize()]
+        [TestInitialize]
         public void Initialize()
         {
         }
 
-        [TestCleanup()]
+        [TestCleanup]
         public void Cleanup()
         {
         }
@@ -57,7 +62,68 @@ namespace UnitTest
         [TestMethod]
         public void ReadInputData()
         {
+            var fileReadInputData = container.Resolve<IFileReadAccess<InputData>>(Container.CsvFileReadAccess);
+            var inputPath = Path.Combine(Environment.CurrentDirectory, @"TestData\InputData.csv");
+            var firstRecord = fileReadInputData.ReadFile(inputPath).First();
 
+            Assert.AreEqual(firstRecord.FirstName, "David");
+            Assert.AreEqual(firstRecord.AnnualSalary, 60050);
+        }
+
+        [TestMethod]
+        public void ReadTaxTable()
+        {
+            var fileReadTaxData = container.Resolve<IFileReadAccess<Tax>>(Container.CsvFileReadAccess);
+            var taxTablePath = Path.Combine(Environment.CurrentDirectory, @"TestData\TaxTable.csv");
+            var lastRecord = fileReadTaxData.ReadFile(taxTablePath).Last();
+
+            Assert.AreEqual(lastRecord.Start, 180001);
+            Assert.AreEqual(lastRecord.Rate, 45);
+        }
+
+        [TestMethod]
+        public void CalculateTax()
+        {
+            var taxManager = container.Resolve<ITaxManager>();
+            var taxList = CreateTaxList();
+
+            Assert.AreEqual(922, taxManager.CalculateTax(taxList, 60050));
+        }
+
+        [TestMethod]
+        public void GenerateOutputData()
+        {
+            var payslipManager = container.Resolve<IPayslipManager>();
+            var taxManager = MockRepository.GenerateStub<ITaxManager>();
+            taxManager.Stub(stub => stub.CalculateTax(null, 0)).IgnoreArguments().Return(922);
+            var inputData = CreateInputData();
+            var outputData = payslipManager.GenerateOutputData(inputData, CreateTaxList(), taxManager);
+
+            Assert.AreEqual(outputData.Name, inputData.FirstName + " " + inputData.LastName);
+            Assert.AreEqual(outputData.GrossIncome, 5004);
+            Assert.AreEqual(outputData.Super, 450);
+            Assert.AreEqual(outputData.NetIncome, 4082);
+        }
+
+        private List<Tax> CreateTaxList()
+        {
+            return new List<Tax>
+                       {
+                           new Tax { Start = 37001, Finish = 80000, Base = 3572, Rate = (decimal)32.5 },
+                           new Tax { Start = 80001, Finish = 180000, Base = 17547, Rate = 37 }
+                       };
+        }
+
+        private InputData CreateInputData()
+        {
+            return new InputData
+            {
+                FirstName = "David",
+                LastName = "Rudd",
+                AnnualSalary = 60050,
+                SuperRate = 9,
+                PaymentStartDate = "01 March - 31 March"
+            };
         }
     }
 }
