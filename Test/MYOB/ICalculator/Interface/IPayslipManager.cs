2113f9fd44d06e775385a48daa560b8b03293804
Steven Liu <steven.liu@catchsoftware.com>
2016-03-09 16:23:49 +1300

    Test Project

--- Test/MYOB/ICalculator/Interface/IPayslipManager.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Calculator.Interface
-{
-    using Castle.Windsor;
-
-    public interface IPayslipManager
-    {
-        void GeneratePayslip(IWindsorContainer container, string inputPath, string outputPath, string taxTablePath);
-    }
-}
